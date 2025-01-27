633789fe19e291ef2861e4293a7111d542e20372
Aleksander Karlsen <aleweskar@gmail.com>
2025-01-27 09:15:45 +0100

    Add required identifikasjon

--- src/main/kotlin/no/kartverket/valgdata/controller/ValgdataController.kt
+++ src/main/kotlin/no/kartverket/valgdata/controller/ValgdataController.kt
@@ -8,99 +8,99 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController("ValgdataController")
 class ValgdataController(private val valgdataService: ValgdataService): ValgdataApi {
-    override fun getTellekretser(): ResponseEntity<InputStreamResource> {
+    override fun getTellekretser(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getTellekretser(framtidig = false)
     }
 
-    override fun getTellekretserKystkontur(): ResponseEntity<InputStreamResource> {
+    override fun getTellekretserKystkontur(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getTellekretserKystkontur(framtidig = false)
     }
 
-    override fun getTellekretserRepresentasjonspunkter(): ResponseEntity<InputStreamResource> {
+    override fun getTellekretserRepresentasjonspunkter(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getTellekretserRepresentasjonspunkter(framtidig = false)
     }
 
-    override fun getTellekretserKystkonturRepresentasjonspunkter(): ResponseEntity<InputStreamResource> {
+    override fun getTellekretserKystkonturRepresentasjonspunkter(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getTellekretserKystkonturRepresentasjonspunkter(framtidig = false)
     }
 
-    override fun getFramtidigeTellekretser(): ResponseEntity<InputStreamResource> {
+    override fun getFramtidigeTellekretser(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getTellekretser(framtidig = true)
     }
 
-    override fun getFramtidigeTellekretserKystkontur(): ResponseEntity<InputStreamResource> {
+    override fun getFramtidigeTellekretserKystkontur(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getTellekretserKystkontur(framtidig = true)
     }
 
-    override fun getFramtidigeTellekretserRepresentasjonspunkter(): ResponseEntity<InputStreamResource> {
+    override fun getFramtidigeTellekretserRepresentasjonspunkter(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getTellekretserRepresentasjonspunkter(framtidig = true)
     }
 
-    override fun getFramtidigeTellekretserKystkonturRepresentasjonspunkter(): ResponseEntity<InputStreamResource> {
+    override fun getFramtidigeTellekretserKystkonturRepresentasjonspunkter(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getTellekretserKystkonturRepresentasjonspunkter(framtidig = true)
     }
 
-    override fun getFylker(): ResponseEntity<InputStreamResource> {
+    override fun getFylker(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getFylker(framtidig = false)
     }
 
-    override fun getFylkerKystkontur(): ResponseEntity<InputStreamResource> {
+    override fun getFylkerKystkontur(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getFylkerKystkontur(framtidig = false)
     }
 
-    override fun getFylkerRepresentasjonspunkter(): ResponseEntity<InputStreamResource> {
+    override fun getFylkerRepresentasjonspunkter(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getFylkerRepresentasjonspunkter(framtidig = false)
     }
 
-    override fun getFylkerKystkonturRepresentasjonspunkter(): ResponseEntity<InputStreamResource> {
+    override fun getFylkerKystkonturRepresentasjonspunkter(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getFylkerKystkonturRepresentasjonspunkter(framtidig = false)
     }
 
-    override fun getFramtidigeFylker(): ResponseEntity<InputStreamResource> {
+    override fun getFramtidigeFylker(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getFylker(framtidig = true)
     }
 
-    override fun getFramtidigeFylkerKystkontur(): ResponseEntity<InputStreamResource> {
+    override fun getFramtidigeFylkerKystkontur(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getFylkerKystkontur(framtidig = true)
     }
 
-    override fun getFramtidigeFylkerRepresentasjonspunkter(): ResponseEntity<InputStreamResource> {
+    override fun getFramtidigeFylkerRepresentasjonspunkter(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getFylkerRepresentasjonspunkter(framtidig = true)
     }
 
-    override fun getFramtidigeFylkerKystkonturRepresentasjonspunkter(): ResponseEntity<InputStreamResource> {
+    override fun getFramtidigeFylkerKystkonturRepresentasjonspunkter(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getFylkerKystkonturRepresentasjonspunkter(framtidig = true)
     }
 
-    override fun getKommuner(): ResponseEntity<InputStreamResource> {
+    override fun getKommuner(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getKommuner(framtidig = false)
     }
 
-    override fun getKommunerKystkontur(): ResponseEntity<InputStreamResource> {
+    override fun getKommunerKystkontur(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getKommunerKystkontur(framtidig = false)
     }
 
-    override fun getKommunerRepresentasjonspunkter(): ResponseEntity<InputStreamResource> {
+    override fun getKommunerRepresentasjonspunkter(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getKommunerRepresentasjonspunkter(framtidig = false)
     }
 
-    override fun getKommunerKystkonturRepresentasjonspunkter(): ResponseEntity<InputStreamResource> {
+    override fun getKommunerKystkonturRepresentasjonspunkter(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getKommunerKystkonturRepresentasjonspunkter(framtidig = false)
     }
 
-    override fun getFramtidigeKommuner(): ResponseEntity<InputStreamResource> {
+    override fun getFramtidigeKommuner(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getKommuner(framtidig = true)
     }
 
-    override fun getFramtidigeKommunerKystkontur(): ResponseEntity<InputStreamResource> {
+    override fun getFramtidigeKommunerKystkontur(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getKommunerKystkontur(framtidig = true)
     }
 
-    override fun getFramtidigeKommunerRepresentasjonspunkter(): ResponseEntity<InputStreamResource> {
+    override fun getFramtidigeKommunerRepresentasjonspunkter(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getKommunerRepresentasjonspunkter(framtidig = true)
     }
 
-    override fun getFramtidigeKommunerKystkonturRepresentasjonspunkter(): ResponseEntity<InputStreamResource> {
+    override fun getFramtidigeKommunerKystkonturRepresentasjonspunkter(identifikasjon: String): ResponseEntity<InputStreamResource> {
         return valgdataService.getKommunerKystkonturRepresentasjonspunkter(framtidig = true)
     }
 
