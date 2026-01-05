cbfbd0d9678045af035f516a1e8cefa977a05407
Kristian Marison Haugerud <kristian.haugerud@kartverket.no>
2026-01-05 12:30:54 +0100

    Spring doc 3.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,15 +19,15 @@ repositories {
 }
 
 // Dependency versions
-val SPRINGDOC_OPENAPI_VERSION = "2.8.15"
+val SPRINGDOC_OPENAPI_VERSION = "3.0.0"
 val MICROMETER_PROMETHEUS_VERSION = "1.16.1"
 val LOGSTASH_VERSION = "9.0"
 val GOOGLE_CLOUD_STORAGE_VERSION = "2.61.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("tools.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
